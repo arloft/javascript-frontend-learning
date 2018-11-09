c06f2b84dc39428260d54b147f21c9b0833e731e
arloft <get@aaronarlof.me>
2018-11-09 16:46:39 -0500

    wrote a single object property to the page (looped)
    Firgured out how to get the loop to write to the page instead of just the console! Was using 'textContent' instead of 'innerHTML' when trying to 'appendChild' a new P element onto a DIV (textContent doesn't create a Node, so appendChild doesn't have a valid reference of where it can attach.

--- jsobjects/scryfall-app/script.js
+++ jsobjects/scryfall-app/script.js
@@ -3,11 +3,16 @@ const logo = document.createElement('img');
 logo.src = 'logo.png';
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
+
 app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
 // Create a request variable and assign a new XMLHttpRequest object to it.
+
+//	container.textContent = "this is a js-generated P element";
+//  This ^^ line writes directly under the 'body' element.
+
 
 request.open('GET', 'https://api.scryfall.com/symbology', true);
 // Open a new connection, using a GET request on the URL endpoint
@@ -15,30 +20,27 @@ request.open('GET', 'https://api.scryfall.com/symbology', true);
 
 request.onload = function () {
   // Begin accessing JSON data here, using a generic function
-  var aSymbol = JSON.parse(this.responseText);
-for (var i = 0; i < aSymbol.data.length; i++) {
-    // ^^ this loop counts the number of array elements (objects)
-    app.appendChild(container).textContent = aSymbol.data[i].english;
-    console.log(aSymbol.data[i].english);
-    // ^^ this prints each array element to the console, using the var i,
-    // to know what number element it's on
-    // ^^ the ".dot-part" after data[i] (.symbol, .english, etc), 
-    // tells what property of the specific object to get 
-  }
+	var mtgdata = JSON.parse(this.responseText);
+	
+	
+	console.log(mtgdata); // View the entire JSON object
+	
+//	console.log(mtgdata.data.map (d => d.english) );
+  
+//	Below is the "long hand" version of ^^ the d => d.english function
+  mtgdata.data.map (function(d) {
+	  
+//	  console.log(container);
+	  const newP = document.createElement('p');
+//	  console.log(newP);
+	  newP.innerHTML = d.english;
+	  // Use 'innerHTML' instead of 'textContent' when creating a new page element, so you have a Node where you can attach new elements
+	  document.body.appendChild(newP);
+	  return d.english;
+  }) ;
+	
+} // End of "request.onload" function
 
-  // if (request.status >= 200 && request.status < 400) {
-    // data.forEach(cardSymbol => {
-  //     // Log for each movie's title
-  //     console.log(cardSymbol.list.object);
-  //     console.log(cardSymbol.list.symbol);
-  //
-  //   });
-  // } else {
-  //   console.log('error');
-    // const errorMessage = document.createElement('marquee');
-    // errorMessage.textContent = `FAIL! Not working :(`;
-    // app.appendChild(errorMessage);
-}
 // }
 
 request.send();
